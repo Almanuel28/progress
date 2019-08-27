2c382f19f923d29ed95bf0698d62d300846b9ee9
Almanuel <almanueldelossantos23@gmail.com>
2019-08-27 15:35:03 +0800

    town

---
+++ sample4.java
@@ -0,0 +1,20 @@
+
+public class sample4{
+
+	public static void main(String[] args) {
+		System.out.println("Hello mga tanga");
+		String ss = "tanga tanga";
+		System.out.println(ss);
+
+		if (ss == "asd") {
+			System.out.println("uto uto mo");
+			
+		}
+		else{
+			System.out.println("nc3");
+		}
+		
+
+		
+	}
+}
